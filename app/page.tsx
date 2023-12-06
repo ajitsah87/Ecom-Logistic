ed4e54e0741f6a79e0fa3b0c42544499768bad5f
ajisah87 <sahajit119@gmail.com>
2023-12-06 21:33:33 +0530

    refacter

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   return (
     <div className="">
-      {/* <Navbar/> */}
+      <Navbar/>
       <div className="h-80 grid place-content-center bg-zinc-800">
         <h1 className="text-5xl font-bold text-white">Please Buy Everything</h1>
       </div>

d7a8d7a6f03c33d92adaa316f3636644a0652cf7
ajisah87 <sahajit119@gmail.com>
2023-12-08 20:48:42 +0530

    ye le

--- app/components/Homee.tsx
+++ app/components/Homee.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import { useGetAllProductsQuery } from "@/app/redux/services/productsApi";
-function Homee() {
-const data=()=>{
 
-    
-}
+function Homee() {
+
 
 
   return (
